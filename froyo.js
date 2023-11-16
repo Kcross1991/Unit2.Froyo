236554c729930e1e1f7f9ca3cc324ebcb506f4f4
root <root@Kacy>
2023-11-16 12:04:55 -0600

    Froyo JS

---
+++ froyo.js
@@ -0,0 +1,9 @@
+const userInputString = prompt("Please enter your froyo flavors separted by commas")
+
+ const flavors={
+    Vanilla: "three",
+    Strawberry: "one",
+    Coffee: "two"
+ }
+ console.table(Object.keys(flavors));
+ console.table(Object.values(flavors));
