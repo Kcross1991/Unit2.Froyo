9862ce95b593732bb0c68bba3b0076b67749021e
root <root@Kacy>
2023-11-16 14:34:03 -0600

    Froyo.js

--- froyo.js
+++ froyo.js
@@ -1,9 +1,35 @@
-const userInputString = prompt("Please enter your froyo flavors separted by commas")
+const userInputString = prompt(
+    "Please enter flavors (Vanilla,Coffee,Strawberry)separated by commas.",
+    "Vaniila,Coffee,Strawberry"
+  );
+  
+  // Split the string of numbers into an array of strings.
+  const stringArray = userInputString.split(",")
 
- const flavors={
-    Vanilla: "three",
-    Strawberry: "one",
-    Coffee: "two"
- }
- console.table(Object.keys(flavors));
- console.table(Object.values(flavors));
+  
+  //A loop is used to iterate through the array of flavors.
+  const counts= {}
+    for (const key of stringArray) {
+        counts[key] = counts[key] ? counts [key] + 1 : 1
+    }
+  
+    console.table(counts)
+
+
+
+
+
+ // let Vaniila=0
+ // let Strawberry=0
+ // let coffee=0
+ // while (stringArray) {
+ //   if (stringArray == 'vanilla')
+  //  {vanilla++
+ //   }
+//if (stringArray=='strawberry')
+  //  {strawberry++
+  //  }
+ //  if(stringArray == 'coffee')
+  //  { coffee++
+  // }
+ //}
